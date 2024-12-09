97cd58f085527848eae41663f5866c5bb196c7d7
Lester Lopez <lopezl10@autonation.com>
2024-12-08 21:31:24 -0500

    puzzle2

--- 2/puzzle_2_2.py
+++ 2/puzzle_2_2.py
@@ -7,32 +7,51 @@ file_path = os.path.join(script_dir, "input2.txt")
 f = open(file_path, "r")
 safeReports = 0
 for line in f:
+    print(line)
     levels = line.split(' ')
+
+    position = 0
+    totalLevels = len(levels)
     safeReport = 1
-    level = 0
-    countBadIncreaseDecrease = 0
-    countIncrease = 0
-    countDecrease = 0
 
-    while (level < len(levels)-1):
-        badIncreaseDecrease, pattern = puzzle_2_module.validateSafeReport(int(levels[level]), int(levels[level+1]))
-        if badIncreaseDecrease:
-            countBadIncreaseDecrease += 1
+    validatingLevels = True
 
-        if pattern == "increase":
-            countIncrease += 1
-        if pattern == "decrease":
-            countDecrease += 1
-        if (countIncrease > 1 and countDecrease > 0) or (countIncrease > 0 and countDecrease > 1):
-            countBadIncreaseDecrease += 1
+    while validatingLevels:
 
-        if countBadIncreaseDecrease > 1:
-            safeReport = 0
+        level = 0
+        countBadIncreaseDecrease = 0
+        countIncrease = 0
+        countDecrease = 0
+
+        while (level < len(levels)-1):
+            badIncreaseDecrease, pattern = puzzle_2_module.validateSafeReport(int(levels[level]), int(levels[level+1]))
+            if badIncreaseDecrease:
+                safeReport = 0
+                break
+            if pattern == "increase":
+                countIncrease += 1
+            if pattern == "decrease":
+                countDecrease += 1
+            if (countIncrease > 1 and countDecrease > 0) or (countIncrease > 0 and countDecrease > 1):
+                safeReport = 0
+                break
+            level += 1
+
+        if safeReport == 1:
             break
-        level += 1
+
+
+        if position > totalLevels - 1:
+            validatingLevels = False
+        else:
+            safeReport = 1
+            levels = line.split(' ')
+            del levels[position]
+            position += 1
 
     safeReports += safeReport
 
 print(safeReports)
+
 
 f.close()
